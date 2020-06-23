bd98a6e14f92ae4a4fab2f47ec063e8324898795
Tuan Phan <tphan@seattleu.edu>
2020-06-22 23:27:22 -0700

    Added Palindrome question

---
+++ Primitives/Palindrome.java
@@ -0,0 +1,23 @@
+
+class Palindrome {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        double log = Math.log10(x);
+        int totalDigits = (int) (Math.floor(log)) + 1;
+        int mask = (int) Math.pow(10, totalDigits - 1);
+
+        for (int i = 0; i < totalDigits / 2; i++) {
+            int first = x / mask;
+            int last = x % 10;
+            if (first != last)
+                return false;
+            x %= mask;
+            x /= 10;
+            mask /= 100;
+        }
+
+        return true;
+    }
+}

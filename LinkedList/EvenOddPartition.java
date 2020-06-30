9827d818c1e41d1fda9b45eaf64d336a80c17f60
Tuan Phan <tphan@seattleu.edu>
2020-06-30 02:05:57 -0700

    Added EvenOddPartition question

---
+++ LinkedList/EvenOddPartition.java
@@ -0,0 +1,24 @@
+package LinkedList;
+
+public class EvenOddPartition {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode even = head;
+        ListNode odd = head.next;
+
+        ListNode oddHead = odd;
+
+        while (odd != null && odd.next != null) {
+            even.next = odd.next;
+            even = odd.next;
+            odd.next = even.next;
+            odd = even.next;
+        }
+
+        even.next = oddHead;
+
+        return head;
+    }
+}

c7796addd089fa1b516c0c087aedfb3365176bc8
Tuan Phan <tphan@seattleu.edu>
2020-07-07 15:58:30 -0700

    Added BalancedParatheses question

--- Stack&Queue/BalancedParatheses.java
+++ Stack&Queue/BalancedParatheses.java
@@ -1,5 +1,49 @@
 package Stack&Queue;
 
-public class BalancedParatheses {
+import java.util.*;
 
+/*
+You are given a string with 3 types of tokens, each with an "open" type and "closed" type.
+
+A string is "balanced" if all open tokes of a certain type are closed appropriately.
+
+Given a string s, return true if it is balanced, return false otherwise.
+
+Opening tokens:
+An open parenthesis (
+An open bracket [
+An open brace {
+
+Closing tokens:
+A closing parenthesis )
+A closing bracket ]
+A closing brace }
+*/
+
+public class BalancedParatheses {
+    public boolean isValid(String s) {
+        Map<Character, Character> MapCloseToOpen = new HashMap<Character, Character>();
+        Stack<Character> stack = new Stack<Character>();
+
+        MapCloseToOpen.put(')', '(');
+        MapCloseToOpen.put('}', '{');
+        MapCloseToOpen.put(']', '[');
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (MapCloseToOpen.containsKey(c)) {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    if (stack.pop() != MapCloseToOpen.get(c)) {
+                        return false;
+                    }
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
 }

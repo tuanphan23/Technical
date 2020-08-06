b08855fd8c677519a8688e4d402f411c1bc7bd9b
Tuan Phan <tphan@seattleu.edu>
2020-08-06 02:03:04 -0700

    Added MinimumItemInRotatedArray question

---
+++ Searching/MinimumItemInRotatedArray.java
@@ -0,0 +1,28 @@
+package Searching;
+
+import java.util.*;
+
+/*
+Given a rotated sorted array, find the minimum element.
+
+A "rotated" array is an array that has had each item shifted to the left or right by k units 
+(where k is a positive integer) while maintaining positional integrity of every element in the original array.
+*/
+
+public class MinimumItemInRotatedArray {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.lenght - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[right])
+                left = mid + 1;
+            if (nums[mid] < nums[right])
+                right = mid;
+        }
+        if (nums.length != 0) {
+            return nums[left];
+        } else
+            return -1;
+    }
+}

38e2d393393f78f9a3476815b705d85fe441e979
Tuan Phan <tphan@seattleu.edu>
2020-08-06 02:04:23 -0700

    Update MinimumItemInRotatedArray question

--- Searching/MinimumItemInRotatedArray.java
+++ Searching/MinimumItemInRotatedArray.java
@@ -12,7 +12,7 @@ A "rotated" array is an array that has had each item shifted to the left or righ
 public class MinimumItemInRotatedArray {
     public int findMin(int[] nums) {
         int left = 0;
-        int right = nums.lenght - 1;
+        int right = nums.length - 1;
         while (left < right) {
             int mid = (left + right) / 2;
             if (nums[mid] > nums[right])
